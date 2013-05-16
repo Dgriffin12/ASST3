42bacc897a5aa3f2943514b19aa3738c692e8e16
Douglas B. Griffin <dbgriffin@o251-07.ecst.csuchico.edu>
2013-05-15 18:25:07 -0700

    Added wrong file last time, this is the correct one, work on ASST3, got a start, could not figure out file permissions and why I could not mkdir in my directory that I was

--- ASST3.c
+++ ASST3.c
@@ -1,3 +1,13 @@
+/*
+  FUSE: Filesystem in Userspace
+  Copyright (C) 2001-2007  Miklos Szeredi <miklos@szeredi.hu>
+
+  This program can be distributed under the terms of the GNU GPL.
+  See the file COPYING.
+
+  gcc -Wall hello.c `pkg-config fuse --cflags --libs` -o hello
+*/
+
 #define FUSE_USE_VERSION 26
 
 #include <fuse.h>
@@ -6,38 +16,46 @@
 #include <errno.h>
 #include <fcntl.h>
 
-const int PATH_MAX = 30;
+static const char *hello_str = "Hello World!\n";
+static const char *hello_path = "/hello";
+static const char *Author_Info = "//Doug Griffin\n //CSCI 340 - Operating Systems\n"; //Wanted to make it so I could include a command that would add this at the beginning of a file.
 
-struct fs_state
-{
-    char* root_dir;
-    char* file_found;
-};
-
-void get_full_path(char full_path[PATH_MAX], const char *path)
-{
-    strcpy(full_path, root_dir);
-    strncat(full_path, path, PATH_MAX);
-}
-
-int search_getattr(const char *path, struct stat *stbuf)
+static int hello_getattr(const char *path, struct stat *stbuf)
 {
         int res = 0;
-        char full_path[PATH_MAX];
-        
-        get_full_path(full_path, path);
-        
-        res = lstat(full_path, stbuf);
-        if(res != 0)
-        {
-            return -ENOENT;
-        }
-        
+
+        memset(stbuf, 0, sizeof(struct stat));
+        if (strcmp(path, "/") == 0) {
+                stbuf->st_mode = S_IFDIR | 0755;
+                stbuf->st_nlink = 2;
+        } else if (strcmp(path, hello_path) == 0) {
+                stbuf->st_mode = S_IFREG | 0444;
+                stbuf->st_nlink = 1;
+                stbuf->st_size = strlen(hello_str);
+        } else
+                res = -ENOENT;
+
         return res;
 }
 
-static int search_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-                         off_t offset, struct fuse_file_info *fi)
+int hello_chmod(const char *path, mode_t mode) //to change permissions
+{
+    int res = 0;
+    res = chmod(path, 777);
+    return res;
+}
+
+int hello_create(const char *path, mode_t mode, struct fuse_file_info *fi) //to create a file
+{
+    int res = 0;
+    int fd;
+    fd = creat(path, mode);
+    fi->fh = fd;
+    return res;
+}
+
+static int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+                         off_t offset, struct fuse_file_info *fi) //readdir unchanged from hello
 {
         (void) offset;
         (void) fi;
@@ -47,52 +65,80 @@ static int search_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 
         filler(buf, ".", NULL, 0);
         filler(buf, "..", NULL, 0);
-        filler(buf, search_path + 1, NULL, 0);
+        filler(buf, hello_path + 1, NULL, 0);
 
         return 0;
 }
 
-static int search_read(const char *path, char *buf, size_t size, off_t offset,
-                      struct fuse_file_info *fi)
-{
-        int res = 0;
-        
-        res = pread(fi->fh, buf, size, offset); //use pread so we don't mess with the file pointer.
-        return res;
-}
-
-
-static int search_open(const char *path, struct fuse_file_info *fi)
+static int hello_open(const char *path, struct fuse_file_info *fi) //opening a file
 {
         int res = 0;
         int fd;
-        char full_path[PATH_MAX];
-        
-        get_full_path(full_path, path);
-        fd = open(full_path, fi->flags);
-        if(fd < 0)
-        {
-            res = -ENOENT;
-        }
+        fd = open(path, fi->flags);
         
         fi->fh = fd;
         
+
         return res;
 }
 
-static struct fuse_operations search_dir = {
-        .getattr        = search_getattr,
-        .readdir        = search_readdir,
-        .open           = search_open,
-        .read           = search_read,
+static int hello_mkdir(const char *path, mode_t mode) //making a directory
+{
+    int res = 0;
+    res = mkdir(path, mode);
+    return res;
+}
+
+static int hello_mknod(const char *path, mode_t mode, dev_t dev) //making a file node
+{
+    int res = 0;
+    res = mknod(path, mode, dev);
+    return res;
+}
+    
+static int hello_read(const char *path, char *buf, size_t size, off_t offset, //reading hello
+                      struct fuse_file_info *fi)
+{
+        size_t len;
+        (void) fi;
+        if(strcmp(path, hello_path) != 0)
+                return -ENOENT;
+
+        len = strlen(hello_str);
+        if (offset < len) {
+                if (offset + size > len)
+                        size = len - offset;
+                memcpy(buf, hello_str + offset, size);
+        } else
+                size = 0;
+
+        return size;
+}
+
+static int hello_write(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi) //writing to a file
+{
+    size_t len;
+    (void) fi;
+    len = strlen(hello_str);
+    
+    size = pwrite(fi->fh, Author_Info, size, offset);
+    return size;
+}
+static struct fuse_operations hello_oper = {
+        .getattr        = hello_getattr,
+        .readdir        = hello_readdir,
+        .open           = hello_open,
+        .read           = hello_read,
+        .write          = hello_write,
+        .chmod          = hello_chmod,
+        .mknod          = hello_mknod,
+        .mkdir          = hello_mkdir,
 };
-        
+
 int main(int argc, char *argv[])
 {
-        struct fs_state *state;
-        state = malloc(sizeof(struct fs_state));
-   
-        state->root_dir = realpath(argv[1], NULL); //set the root directory to the first argument(which should be the mount point)
-        state->file_found = realpath(argv[2], NULL); // set file_found to the second argument, which is the file we will be looking for.
-        return fuse_main(argc, argv, &search_dir, NULL);
+        return fuse_main(argc, argv, &hello_oper, NULL);
 }
+
+
+// syntax highlighted by Code2HTML, v. 0.9.1 
